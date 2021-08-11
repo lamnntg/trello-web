51aab98f4d538a638429f09c17a192fdc1d1c494
lamylov3 <lamnguyen.hust@gmail.com>
2021-08-12 01:24:35 +0700

    design trello

--- src/App.js
+++ src/App.js
@@ -1,9 +1,173 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="h1">Hello, world!</h1>
+    <div className="trello-web">
+      <nav className="navbar app">navbar app</nav>
+      <nav className="navbar board">navbar board</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Header</header>
+          <ul>
+            <li>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
+                alt="image"></img>
+              Title
+
+            </li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+          </ul>
+          <footer className="footer">footer</footer>
+        </div>
+
+        <div className="column">
+          <header>Header</header>
+          <ul>
+            <li>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
+                alt="image"></img>
+              Title
+
+            </li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+          </ul>
+          <footer className="footer">footer</footer>
+        </div>
+
+        <div className="column">
+          <header>Header</header>
+          <ul>
+            <li>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
+                alt="image"></img>
+              Title
+
+            </li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+          </ul>
+          <footer className="footer">footer</footer>
+        </div>
+
+        <div className="column">
+          <header>Header</header>
+          <ul>
+            <li>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
+                alt="image"></img>
+              Title
+
+            </li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+          </ul>
+          <footer className="footer">footer</footer>
+        </div>
+
+        <div className="column">
+          <header>Header</header>
+          <ul>
+            <li>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
+                alt="image"></img>
+              Title
+
+            </li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+          </ul>
+          <footer className="footer">footer</footer>
+        </div>
+
+        <div className="column">
+          <header>Header</header>
+          <ul>
+            <li>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
+                alt="image"></img>
+              Title
+
+            </li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+          </ul>
+          <footer className="footer">footer</footer>
+        </div>
+
+        <div className="column">
+          <header>Header</header>
+          <ul>
+            <li>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
+                alt="image"></img>
+              Title
+
+            </li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+          </ul>
+          <footer className="footer">footer</footer>
+        </div>
+
+        <div className="column">
+          <header>Header</header>
+          <ul>
+            <li>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
+                alt="image"></img>
+              Title
+
+            </li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+          </ul>
+          <footer className="footer">footer</footer>
+        </div>
+
+        <div className="column">
+          <header>Header</header>
+          <ul>
+            <li>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
+                alt="image"></img>
+              Title
+
+            </li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+            <li>Almost before we knew it, we had left the ground.</li>
+          </ul>
+          <footer className="footer">footer</footer>
+        </div>
+      </div>
     </div>
   );
 }
